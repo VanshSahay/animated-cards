29acb71a96c3cbfc51db88b9f3318e96b9216c5c
VanshSahay <vansh.sahay1234@gmail.com>
2024-03-21 02:59:44 +0530

    Added hover effect for the cards

--- src/Containers/Header/Header.jsx
+++ src/Containers/Header/Header.jsx
@@ -15,12 +15,13 @@ function Header() {
 
     const carddivs = cards.map(card => 
         <div key={card.id} className='max-h-[700px] max-w-[400px]'>
-            <img src={card.img} alt=".." className='object-cover h-[700px] w-[400px] float-left'/>
+            <div className='h-[700px] w-[400px] rounded-3xl bg-black absolute opacity-50 transition hover:opacity-0 ease-in duration-200'></div>
+            <img src={card.img} alt=".." className='object-cover h-[700px] w-[400px] float-left rounded-3xl'/>
         </div>    
     )
 
   return (
-    <div className='flex justify-around h-screen items-center bg-gradient-to-t from-[#0D1F23] via-[#2D4A53] to-[#69818D]'>{carddivs}</div>
+    <div className='flex justify-around h-screen items-center bg-gradient-to-t from-[#050f19] to-[#2386eb]'>{carddivs}</div>
   )
 }
 
