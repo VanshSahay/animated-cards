840bb015c183a93d1c95eddc30a69bce4a98e29d
VanshSahay <vansh.sahay1234@gmail.com>
2024-03-22 18:45:43 +0530

    completed animations on hovering

--- src/Containers/Header/Header.jsx
+++ src/Containers/Header/Header.jsx
@@ -14,10 +14,13 @@ function Header() {
     }];
 
     const carddivs = cards.map(card => 
-        <div key={card.id} className='max-h-[700px] max-w-[400px]'>
-            <div className='h-[700px] w-[400px] rounded-3xl bg-black absolute opacity-50 transition hover:opacity-0 ease-in duration-200'></div>
-            <img src={card.img} alt=".." className='object-cover h-[700px] w-[400px] float-left rounded-3xl'/>
-        </div>    
+        <div key={card.id} className='max-h-[700px] max-w-[400px] flex justify-center items-center group'>
+            <img src={card.img} alt=".." className='object-cover h-[700px] w-[400px] float-left rounded-3xl absolute pointer-events-none'/>
+            <div className='h-[700px] w-[400px] rounded-3xl bg-black absolute opacity-50 transition group-hover:opacity-0 ease-in duration-200'></div>
+            <div className=' min-h-[700px] min-w-[400px] flex justify-center relative transition opacity-0 group-hover:opacity-100 hover:-translate-y-16 ease-out duration-300'>
+                <h1 className='text-4xl font-bold w-fit h-fit'>Click Me!</h1>
+            </div>
+        </div>   
     )
 
   return (
